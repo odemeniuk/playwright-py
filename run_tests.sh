c352732260efcff1eaca9deda81dd0c3318affc1
Oleksandr Demeniuk <demeniuk@gmail.com>
2020-08-02 15:56:57 -0500

    updated allure report integration

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,7 @@ pipenv update
 pipenv run python -m playwright install
 
 echo "-> Removing old Allure results"
+#rm -r allure-report
 rm -r allure-results/ || echo "No results"
 
 echo "-> Start tests"
@@ -11,5 +12,7 @@ pipenv run pytest test --alluredir allure-results
 echo "-> Test finished"
 
 echo "-> Generating report"
-allure generate allure-results --clean -o allure-report
+allure serve
+#allure generate allure-results --clean -o allure-report
 echo "-> Execute 'allure serve' in the command line"
+
